c8465c27c7c6992c942c53301a04c071fda57842
Hzet <jedjas7@gmail.com>
2020-12-03 20:00:02 +0100

    Added event information broker object

---
+++ AbstractedGL/source/System/event.hpp
@@ -0,0 +1,59 @@
+#pragma once
+
+#include "event-types.hpp"
+
+namespace agl
+{
+	struct SEvent // alternative name: Event
+	{
+		struct SPosition
+		{
+			double x;
+			double y;
+		};
+
+		struct SSize
+		{
+			std::uint32_t width;
+			std::uint32_t height;
+		};
+
+		struct SScroll
+		{
+			double x;
+			double y;
+		};
+
+		struct SKeyboard
+		{
+			Event::Code code;
+			std::int32_t scancode;
+			std::int32_t modifiers;
+		};
+
+		struct SMouse
+		{
+			Event::EButton code;
+			std::int32_t bit_modifiers;
+		};
+
+		struct SScale
+		{
+			float x;
+			float y;
+		};
+
+		Event::EType type = Event::INVALID_EVENT;
+
+		union
+		{
+			SPosition	   position;
+			SSize		   size;
+			SScroll		   scroll;
+			SKeyboard	   key;
+			SMouse		   button;
+			SScale		   scale;
+			std::uint32_t  character;
+		};
+	};
+}
