22025268f5be8d9ef787c06bb8db344582fc67f9
Hzet <jedjas7@gmail.com>
2020-12-04 01:28:29 +0100

    Rewrote macros to serve for more versitality

--- AbstractedGL/source/System/log.hpp
+++ AbstractedGL/source/System/log.hpp
@@ -31,24 +31,31 @@ namespace agl
 #include "log.inl"
 }
 
+#define AGL_BEGIN_TAG(tag) "\n---------------------------------", tag, " BEGIN", "---------------------------------\n"
+#define AGL_END_TAG(tag) "\n---------------------------------", tag, " END", "---------------------------------\n"
+
 #define AGL_LOG(message, ...) agl::CLog::SendMessage(x, __VA_ARGS__)
 
-#define AGL_ERROR_LOG(message, ...) \
-	do { \
-		std::ofstream file("error-dump.log", std::ios::out | std::ios::app); \
+#define AGL_LOG_TAGGED(tag, message, ...) agl::CLog::SendMessage(AGL_BEGIN_TAG(tag), message, __VA_ARGS__, AGL_END_TAG(tag))
+
+#define AGL_LOG_FILE(filename, message, ...) \
+	{ \
+		std::ofstream file(filename, std::ios::out | std::ios::app); \
 		agl::CLog::SendOneMessage(file, message, __VA_ARGS__); \
-	} while(false) \
+		file.close(); \
+	} \
+
+#define AGL_LOG_FILE_TAGGED(filename, tag, message, ...) AGL_LOG_FILE(filename, AGL_BEGIN_TAG(tag), message, __VA_ARGS__, AGL_END_TAG(tag));
+
+#define AGL_LOG_STREAM(stream, message, ...) agl::CLog::SendOneMessage(stream, message, __VA_ARGS__)
+
+#define AGL_LOG_STREAM_TAGGED(stream, tag, message, ...)  agl::CLog::SendOneMessage(stream, AGL_BEGIN_TAG(tag), message, __VA_ARGS__, AGL_END_TAG(tag))
+
+#define AGL_ERROR_LOG(message, ...) AGL_LOG_FILE_TAGGED("error-dump.log", AGL_CODE_NAME_COMA, message, __VA_ARGS__)
+
+#define AGL_ERROR_LOG_STREAM(stream, message, ...) AGL_LOG_STREAM(stream, AGL_CODE_NAME_COMA, message, __VA_ARGS__)
 
 #ifdef AGL_DEBUG
-	#define AGL_DEBUG_LOG_IMPL(...) \
-		do { \
-			std::ofstream file("debug.log", std::ios::out | std::ios::app); \
-			agl::CLog::SendOneMessage(file, __VA_ARGS__); \
-		} while(false) \
-
-	#define AGL_DEBUG_LOG(message, ...) \
-		do { \
-			AGL_DEBUG_LOG_IMPL(AGL_CODE_NAME_COMA, message, __VA_ARGS__); \
-		} while(false) \
+#define AGL_DEBUG_LOG(message, ...) AGL_LOG_FILE_TAGGED("debug-dump.log", AGL_CODE_NAME_COMA, message, __VA_ARGS__)
 
 #endif
