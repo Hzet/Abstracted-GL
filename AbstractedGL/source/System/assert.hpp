cc281819436f39eed57b20e06ba5e83701e8b376
Hzet <jedjas7@gmail.com>
2020-12-06 20:19:54 +0100

    Added updated assertions

--- AbstractedGL/source/System/assert.hpp
+++ AbstractedGL/source/System/assert.hpp
@@ -1,26 +1,24 @@
 #pragma once
+
 #include "log.hpp"
 #include "debug.hpp"
 
 #ifdef AGL_DEBUG
-	#define AGL_CORE_ASSERT(condition, ...) AGL_ASSERT_IMPL(condition, "CORE ASSERT", __VA_ARGS__ )
-
-	#define AGL_ASSERT(condition, ...) AGL_ASSERT_IMPL(condition, "ASSERT", __VA_ARGS__)
-
-	#define AGL_ASSERT_IMPL(condition, tag, message, ...) \
-		do \
-		{ \
-			if(!condition) { \
-				AGL_DEBUG_LOG_TAGGED("debug-dump.log", tag, message, __VA_ARGS__); \
+	#define AGL_CORE_ASSERT(condition, ...) \
+		do { \
+			if(!condition) \
+			{ \
+				AGL_CORE_DEBUG("Assertion triggered at:\n", AGL_CODE_POINT, "On: ", #condition, "\n", __VA_ARGS__); \
 				AGL_HALT(); \
 			} \
-		} while(0) \
+		} while(false)
 
-#else
-	#define AGL_ASSERT_IMPL(...) \
-		do \
-		{ \
-			(void)sizeof(condition); \
-		} while(0) \
-
-#endif 
+	#define AGL_CORE_ASSERT(condition, ...) \
+			do { \
+				if(!condition) \
+				{ \
+					AGL_DEBUG("Assertion triggered at:\n", AGL_CODE_POINT, "On: ", #condition, "\n", __VA_ARGS__); \
+					AGL_HALT(); \
+				} \
+			} while(false)
+#endif
