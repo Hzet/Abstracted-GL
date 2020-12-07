0f63b798b260ba0dc2acaf1b598de4524cd925e8
Hzet <jedjas7@gmail.com>
2020-12-07 19:17:55 +0100

    Added macros to check return values and report errors

--- AbstractedGL/source/System/error.hpp
+++ AbstractedGL/source/System/error.hpp
@@ -1,17 +1,39 @@
 #pragma once 
 
-#include "exception.hpp"
 #include "log.hpp"
+#include "assert.hpp"
+#include "exception.hpp"
 
-#define AGL_CRITICAL_ERROR_IMPL(condition, tag, message, ...) \
-	do { \
-		if(!condition) \
+#ifdef AGL_DEBUG
+	#define AGL_CORE_CRITICAL(call, comparison, expected, message, ...) \
 		{ \
-			AGL_ERROR_LOG_TAGGED(tag, "Critical error on: ", #condition, message, __VA_ARGS__); \
-			throw agl::exception::CException(message); \
-		} \
-	} while(false) \
+			auto result = call; \
+			AGL_CORE_ASSERT(result comparison call, "Unrecoverable error has occured!\nExpected: [", result, " ", #comparison, " ", expected,  "]\nResult: [", result, "]\nDescription: ", message, __VA_ARGS__); \
+		}
 
-#define AGL_CRITICALL_ERROR_CORE(condition, message, ...) AGL_CRITICAL_ERROR_IMPL(condition, "CRITICAL CORE ERROR", message, __VA_ARGS__);
+	#define AGL_CRITICAL(call, comparison, expected, message, ...) \
+		{ \
+			auto result = call; \
+			AGL_ASSERT(result comparison call, "Unrecoverable error has occured!\nExpected: [", result, " ", #comparison, " ", expected,  "]\nResult: [", result, "]\nDescription: ", message, __VA_ARGS__); \
+		}
+#else
+	#define AGL_CORE_CRITICAL(call, comparison, expected, message, ...) \
+		{ \
+			auto result = call; \
+			if(!(result comparison expected)) \
+			{ \
+				AGL_CORE_LOG_CRITICAL("Call to: ", #call, " has failed!\nExpected: [", expected, "]\nResult: [", result, "]\nDescription: ", message, __VA_ARGS__); \
+				throw agl::exception::CException("Unrecoverable error has occured!"); \
+			} \
+		}
 
-#define AGL_CRITICAL_ERROR(condition, message, ...) AGL_CRITICAL_ERROR_IMPL(condition, "CRITICAL ERROR", message, __VA_ARGS__);
+	#define AGL_CRITICAL(call, comparison, exprected, message, ...) \
+		{ \
+			auto result = call; \			
+			if(!(result comparison expected)) \
+			{ \
+				AGL_LOG_CRITICAL("Call to: ", #call, " has failed!\nExpected: [", expected, "]\nResult: [", result, "]\nDescription: ", message, __VA_ARGS__); \
+				throw agl::exception::CException("Unrecoverable error has occured!"); \
+			} \
+		}
+#endif
