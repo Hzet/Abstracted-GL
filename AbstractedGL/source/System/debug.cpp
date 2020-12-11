2ada10d63a5103a6b80e566e5cba55c307204196
Hzet <jedjas7@gmail.com>
2020-12-11 16:53:21 +0100

    Minor style issues

--- AbstractedGL/source/System/debug.cpp
+++ AbstractedGL/source/System/debug.cpp
@@ -2,22 +2,24 @@
 
 namespace agl
 {
-	std::unordered_map<std::string, std::uint64_t> CProfilerClock::Clocks_ = {};
-
-	const std::unordered_map<std::string, std::uint64_t>& CProfilerClock::GetClocks()
+	namespace system
 	{
-		return Clocks_;
-	}
+		std::unordered_map<std::string, std::uint64_t> CProfilerClock::Clocks_ = {};
 
-	CProfilerClock::CProfilerClock(std::string &&callerName)
-		: name_(std::move(callerName)),
-		myClock_()
-	{
-	}
+		const std::unordered_map<std::string, std::uint64_t>& CProfilerClock::GetClocks()
+		{
+			return Clocks_;
+		}
 
-	CProfilerClock::~CProfilerClock()
-	{
-		Clocks_[name_] += myClock_.count();
+		CProfilerClock::CProfilerClock(std::string &&callerName)
+			: name_(std::move(callerName)),
+			myClock_()
+		{
+		}
+
+		CProfilerClock::~CProfilerClock()
+		{
+			Clocks_[name_] += myClock_.count();
+		}
 	}
 }
-
