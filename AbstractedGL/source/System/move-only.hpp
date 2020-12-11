0623ea86cb246e06d24669feb2f582410b7d320d
Hzet <jedjas7@gmail.com>
2020-12-11 01:20:56 +0100

    set copy constructor to be deleted

--- AbstractedGL/source/System/move-only.hpp
+++ AbstractedGL/source/System/move-only.hpp
@@ -9,9 +9,11 @@ namespace agl
 		public:
 			CMoveOnly();
 			CMoveOnly(CMoveOnly &&other);
+			CMoveOnly(const CMoveOnly &) = delete;
 			virtual ~CMoveOnly() = default;
 
 			bool isMoveConstructing() const;
+
 		private:
 			bool move_;
 		};
