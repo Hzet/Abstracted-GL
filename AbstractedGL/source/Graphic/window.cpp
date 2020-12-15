f243e34e16dbffb1809833047f2c90b886a8bb5a
Hzet <jedjas7@gmail.com>
2020-12-15 18:42:18 +0100

    CWindow is now responsible for OpenGL calls

---
+++ AbstractedGL/source/Graphic/window.cpp
@@ -0,0 +1,28 @@
+#include "window.hpp"
+#include "drawable.hpp"
+
+#include "../System/gl-core.hpp"
+
+namespace agl
+{
+	CWindow::CWindow(const std::string &title, const std::uint32_t width, const std::uint32_t height)
+		: CWindowBase(CWindowBase::Create(title, width, height))
+	{
+	}
+
+	void CWindow::draw(const IDrawable &drawable) const
+	{
+		drawable.draw(*this);
+	}
+
+	void CWindow::clear(glm::vec4 color) const
+	{
+		AGL_CALL(glClearColor(color.x, color.y, color.z, color.w));
+	}
+
+	void CWindow::clearBit(std::uint64_t bits) const
+	{
+		AGL_CALL(glClear(bits));
+	}
+}
+
---
+++ AbstractedGL/source/Graphic/window.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "window-base.hpp"
+#include "renderer.hpp"
+
+namespace agl
+{
+	struct IDrawable;
+
+	class CWindow
+		: public CWindowBase,	
+		public CRenderer
+	{
+	public:
+		CWindow(const std::string &title, const std::uint32_t width, const std::uint32_t height);
+
+
+		void draw(const IDrawable &drawable) const;
+
+		void clear(glm::vec4 color) const;
+		void clearBit(std::uint64_t bits) const;
+	private:
+
+	};
+}
