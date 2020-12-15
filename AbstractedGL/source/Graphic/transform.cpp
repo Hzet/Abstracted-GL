edd53dad37df39a570e5da7b79587a70cfe3ab88
Hzet <jedjas7@gmail.com>
2020-12-15 18:43:23 +0100

    Class providing operations to transform 4x4 matrix

---
+++ AbstractedGL/source/Graphic/transform.cpp
@@ -0,0 +1,54 @@
+#include "transform.hpp"
+
+#include <glm/ext.hpp>
+
+namespace agl
+{
+	CTransform::CTransform()
+		: matrix_(1.f)
+	{
+	}
+
+	CTransform::CTransform(const glm::mat4 matrix)
+		: matrix_(matrix)
+	{
+	}
+
+	void CTransform::scale(const glm::vec3 &scale)
+	{
+		matrix_ = glm::scale(matrix_, scale);
+	}
+
+	void CTransform::rotate(const glm::vec3 &angle)
+	{
+		matrix_ = glm::rotate(matrix_, glm::radians(angle.x), { 1.f, 0.f, 0.f });
+		matrix_ = glm::rotate(matrix_, glm::radians(angle.y), { 0.f, 1.f, 0.f });
+		matrix_ = glm::rotate(matrix_, glm::radians(angle.z), { 0.f, 0.f, 1.f });
+	}
+
+	void CTransform::translate(const glm::vec3 &offset)
+	{
+		matrix_ = glm::translate(matrix_, offset);
+	}
+
+	CTransform CTransform::getInverse() const
+	{
+		return glm::inverse(matrix_);
+	}
+
+	CTransform& CTransform::operator*=(const CTransform &other)
+	{
+		matrix_ = matrix_ * other.matrix_;
+		return *this;
+	}
+
+	CTransform CTransform::operator*(const CTransform &other) const
+	{
+		return matrix_ * other.matrix_;
+	}
+
+	CTransform::operator const glm::mat4&() const
+	{
+		return matrix_;
+	}
+}
---
+++ AbstractedGL/source/Graphic/transform.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+namespace agl
+{
+	class CTransform
+	{
+	public:
+		CTransform();
+		CTransform(const glm::mat4 matrix);
+		CTransform(CTransform&&) = default;
+		CTransform(const CTransform&) = default;
+		virtual ~CTransform() = default;
+
+		void scale(const glm::vec3 &scale);
+		void rotate(const glm::vec3 &angle);
+		void translate(const glm::vec3 &offset);
+
+		CTransform getInverse() const;
+
+		operator const glm::mat4&() const;
+		CTransform& operator*=(const CTransform &other);
+		CTransform operator*(const CTransform &other) const;
+
+	private:
+		glm::mat4 matrix_;
+	};
+}
