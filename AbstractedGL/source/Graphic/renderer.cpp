f536086ddac1698f9d487ee1b2ff5b16e9d9713c
Hzet <jedjas7@gmail.com>
2020-12-23 19:56:05 +0100

    Changed asserts to core assert calls

--- AbstractedGL/source/Graphic/renderer.cpp
+++ AbstractedGL/source/Graphic/renderer.cpp
@@ -16,28 +16,28 @@ namespace agl
 
 	void CRenderer::addShader(std::uint32_t name, CShader &&shader)
 	{
-		AGL_ASSERT(shaders_.find(name) == shaders_.end(), "Shader already added!");
+		AGL_CORE_ASSERT(shaders_.find(name) == shaders_.end(), "Shader already added!");
 
 		shaders_[name] = std::move(shader);
 	}
 
 	const CShader& CRenderer::getShader(std::uint32_t name) const
 	{
-		AGL_ASSERT(shaders_.find(name) != shaders_.end(), "Shader does not exist!");
+		AGL_CORE_ASSERT(shaders_.find(name) != shaders_.end(), "Shader does not exist!");
 
 		return shaders_.at(name);
 	}
 
 	const CShader& CRenderer::getActiveShader() const
 	{
-		AGL_ASSERT(shaders_.find(active_) != shaders_.end(), "Shader does not exist!");
+		AGL_CORE_ASSERT(shaders_.find(active_) != shaders_.end(), "Shader does not exist!");
 
 		return shaders_.at(active_);
 	}
 
 	void CRenderer::setActiveShader(std::uint32_t name)
 	{
-		AGL_ASSERT(shaders_.find(name) != shaders_.end(), "Shader does not exist!");
+		AGL_CORE_ASSERT(shaders_.find(name) != shaders_.end(), "Shader does not exist!");
 
 		active_ = name;
 		shaders_.at(name).setActive();
