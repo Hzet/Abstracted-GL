2aceb9f049c3e1b224ffc2a704e696e615591028
Hzet <jedjas7@gmail.com>
2021-02-11 22:48:59 +0100

    The renderer is longer handling the shaders

--- AbstractedGL/source/Graphic/renderer.cpp
+++ AbstractedGL/source/Graphic/renderer.cpp
@@ -13,34 +13,4 @@ namespace agl
 		else
 			AGL_CALL(glDrawElements(drawType, vArray.getIndexCount(), GL_UNSIGNED_INT, 0u));
 	}
-
-	void CRenderer::addShader(std::uint32_t name, CShader &&shader)
-	{
-		AGL_CORE_ASSERT(shaders_.find(name) == shaders_.end(), "Shader already added!");
-
-		shaders_[name] = std::move(shader);
-	}
-
-	const CShader& CRenderer::getShader(std::uint32_t name) const
-	{
-		AGL_CORE_ASSERT(shaders_.find(name) != shaders_.end(), "Shader does not exist!");
-
-		return shaders_.at(name);
-	}
-
-	const CShader& CRenderer::getActiveShader() const
-	{
-		AGL_CORE_ASSERT(shaders_.find(active_) != shaders_.end(), "Shader does not exist!");
-
-		return shaders_.at(active_);
-	}
-
-	void CRenderer::setActiveShader(std::uint32_t name)
-	{
-		AGL_CORE_ASSERT(shaders_.find(name) != shaders_.end(), "Shader does not exist!");
-
-		active_ = name;
-		shaders_.at(name).setActive();
-	}
-
 }
