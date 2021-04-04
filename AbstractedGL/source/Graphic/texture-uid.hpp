bd8c110601c5a831f913e2b83448853d4027c771
Hzet <jedjas7@gmail.com>
2021-04-04 15:07:05 +0200

    Added texture's unique identifier

---
+++ AbstractedGL/source/Graphic/texture-uid.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "../System/unique-identifier.hpp"
+
+namespace agl
+{
+	namespace texture
+	{
+		struct STextureUID {};
+	}
+
+	using CTextureUID = system::CUID<texture::STextureUID>;
+}
+
+namespace std
+{
+	/// <summary>
+	/// The specialized hasher structure.
+	/// </summary>
+	template <>
+	struct hash<agl::CTextureUID>
+	{
+		size_t operator()(const agl::CTextureUID &uid) const noexcept
+		{
+			return hash<uint64_t>{}(static_cast<const uint64_t>(uid));
+		}
+	};
+}
